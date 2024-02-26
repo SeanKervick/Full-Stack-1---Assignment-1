910654216e16edf4eb6ae758751f356388110391
SeanKervick <20104300@mail.wit.ie>
2024-02-26 12:20:48 +0000

    User API

--- test/api/hikeplace-service.js
+++ test/api/hikeplace-service.js
@@ -1,3 +1,27 @@
-export const hikeplaceService = {
+import axios from "axios";
 
+import { serviceUrl } from "../fixtures.js";
+
+export const hikeplaceService = {
+  hikeplaceUrl: serviceUrl,
+
+  async createUser(user) {
+    const res = await axios.post(`${this.hikeplaceUrl}/api/users`, user);
+    return res.data;
+  },
+
+  async getUser(id) {
+    const res = await axios.get(`${this.hikeplaceUrl}/api/users/${id}`);
+    return res.data;
+  },
+
+  async getAllUsers() {
+    const res = await axios.get(`${this.hikeplaceUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteAllUsers() {
+    const res = await axios.delete(`${this.hikeplaceUrl}/api/users`);
+    return res.data;
+  },
 };
